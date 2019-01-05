6761a289d01f7b55dbf2075fbd7b0162ece3be6a
Thiago Adams <thiago.adams@gmail.com>
2019-01-05 16:22:33 -0200

    Update fs.h

--- fs.h
+++ fs.h
@@ -2,6 +2,8 @@
 
 #include <stdbool.h>
 
+#define FS_MAX_PATH 256
+
 struct error_code
 {
     unsigned int code;
@@ -14,6 +16,13 @@ bool fs_copy_file(const char* pathfrom, const char* pathto, struct error_code* e
 bool fs_exists(const char* path, struct error_code* ec);
 bool fs_remove(const char* path, struct error_code* ec);
 
+bool fs_current_path(char* pathOut);
+
+void fs_path_split(const char* Path,
+  char* Drive,
+  char* Directory,
+  char* Filename,
+  char* Extension);
 
 struct directory_iterator
 {
@@ -22,6 +31,8 @@ struct directory_iterator
     bool bIsDir;
 };
 
-bool directory_iterator_init(struct directory_iterator* di, const char* path);
+bool directory_iterator_init(struct directory_iterator* di, const char* path, struct error_code* ec);
 bool directory_iterator_next(struct directory_iterator* di);
 void directory_iterator_destroy(struct directory_iterator* di);
+
+bool  fs_copy(const char* source, const char* dest, struct error_code* ec);
