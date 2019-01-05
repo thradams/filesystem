d28d587fb9ec3a9484f448ffc26d1bf6fdd9ae56
Thiago Adams <thiago.adams@gmail.com>
2019-01-05 16:22:12 -0200

    Update fs.c

--- fs.c
+++ fs.c
@@ -4,8 +4,18 @@
 #include <windows.h>
 #include "Shlwapi.h"
 #include <string.h>
+#include <assert.h>
 
 #pragma comment(lib, "Shlwapi.lib")
+
+void fs_path_split(const char* Path,
+    char* Drive,
+    char* Directory,
+    char* Filename,
+    char* Extension)
+{
+    _splitpath(Path, Drive, Directory, Filename, Extension);
+}
 
 bool fs_remove(const char* path, struct error_code* ec)
 {
@@ -19,12 +29,21 @@ bool fs_exists(const char* path, struct error_code* ec)
 
 bool fs_create_directory(const char* path, struct error_code* ec)
 {
-    if (!CreateDirectoryA(path, NULL))
+    bool b = CreateDirectoryA(path, NULL);
+    if (!b)
     {
-        //GetLastError();
-        return false;
+        ec->code = GetLastError();
+        switch (ec->code)
+        {
+        case ERROR_ALREADY_EXISTS:
+            b = true;
+            break;
+        case ERROR_PATH_NOT_FOUND:
+            break;
+        }
+
     }
-    return true;
+    return b;
 }
 
 
@@ -32,14 +51,20 @@ bool fs_copy_file(const char* pathfrom,
     const char* pathto,
     struct error_code* ec)
 {
-    return CopyFileA(
+    bool b = CopyFileA(
         pathfrom,
         pathto,
         0 /*bFailIfExists*/
-    );
+    ) != 0;
+
+    if (!b)
+    {
+        ec->code = GetLastError();
+    }
+    return b;
 }
 
-bool directory_iterator_init(struct directory_iterator* di, const char* path)
+bool directory_iterator_init(struct directory_iterator* di, const char* path, struct error_code* ec)
 {
     WIN32_FIND_DATAA fdFile;
 
@@ -49,9 +74,17 @@ bool directory_iterator_init(struct directory_iterator* di, const char* path)
 
     di->handle = FindFirstFileA(sPath, &fdFile);
 
-    di->bIsDir = fdFile.dwFileAttributes &FILE_ATTRIBUTE_DIRECTORY;
-    strcpy(di->fileName, fdFile.cFileName);
-    return di->handle != INVALID_HANDLE_VALUE;
+    bool b = di->handle != INVALID_HANDLE_VALUE;
+    if (b)
+    {
+        di->bIsDir = fdFile.dwFileAttributes &FILE_ATTRIBUTE_DIRECTORY;
+        strcpy(di->fileName, fdFile.cFileName);
+    }
+    else
+    {
+        ec->code = GetLastError();
+    }
+    return b;
 }
 
 bool directory_iterator_next(struct directory_iterator* di)
@@ -63,6 +96,10 @@ bool directory_iterator_next(struct directory_iterator* di)
         di->bIsDir = fdFile.dwFileAttributes &FILE_ATTRIBUTE_DIRECTORY;
         strcpy(di->fileName, fdFile.cFileName);
     }
+    else
+    {
+
+    }
     return b;
 }
 
@@ -70,7 +107,25 @@ void directory_iterator_destroy(struct directory_iterator* di)
 {
     FindClose((HANDLE)di->handle);
 }
-
+bool fs_current_path(char* pathOut)
+{
+    /*
+    If the function succeeds, the return value specifies the number
+    of characters that are written to the buffer, not including the
+    terminating null character.
+    If the function fails, the return value is zero.To get extended error
+    information, call GetLastError.
+    If the buffer that is pointed to by lpBuffer is not large enough,
+    the return value specifies the required size of the buffer,
+    in characters, including the null - terminating character.
+    */
+    DWORD r = GetCurrentDirectoryA(MAX_PATH, pathOut);
+    if (r == 0)
+    {
+        //GetLastError();
+    }
+    return r != 0;
+}
 
 #else
 #include <sys/stat.h>
@@ -80,11 +135,18 @@ void directory_iterator_destroy(struct directory_iterator* di)
 #include <errno.h>
 #include <dirent.h>
 #include <string.h>
+#include <unistd.h>
+
 bool fs_create_directory(const char* path, struct error_code* ec)
 {
     //http://pubs.opengroup.org/onlinepubs/009695399/functions/mkdir.html
     //http://www.gnu.org/software/coreutils/mkdir
     return mkdir(path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) == 0;
+}
+
+bool fs_current_path(char* pathOut)
+{
+    return getcwd(pathOut, 256);
 }
 
 bool fs_copy_file(const char* pathfrom,
@@ -162,7 +224,7 @@ bool fs_remove(const char* path, struct error_code* ec)
 }
 
 
-bool directory_iterator_init(struct directory_iterator* di, const char* path)
+bool directory_iterator_init(struct directory_iterator* di, const char* path, struct error_code* ec)
 {
     di->handle = opendir(path);
     if (di->handle)
@@ -189,5 +251,135 @@ void directory_iterator_destroy(struct directory_iterator* di)
     closedir((DIR*)di->handle);
 }
 
+
+
+void fs_path_split(const char* Path,
+    char* Drive,
+    char* Directory,
+    char* Filename,
+    char* Extension)
+{
+    char* CopyOfPath = (char*)Path;
+    int Counter = 0;
+    int Last = 0;
+    int Rest = 0;
+
+    // no drives available in linux .
+    // extensions are not common in linux
+    // but considered anyway
+    Drive = NULL;
+
+    while (*CopyOfPath != '\0')
+    {
+        // search for the last slash
+        while (*CopyOfPath != '/' && *CopyOfPath != '\0')
+        {
+            CopyOfPath++;
+            Counter++;
+        }
+        if (*CopyOfPath == '/')
+        {
+            CopyOfPath++;
+            Counter++;
+            Last = Counter;
+        }
+        else
+            Rest = Counter - Last;
+    }
+    // directory is the first part of the path until the
+    // last slash appears
+    strncpy(Directory, Path, Last);
+    // strncpy doesnt add a '\0'
+    Directory[Last] = '\0';
+    // Filename is the part behind the last slahs
+    strcpy(Filename, CopyOfPath -= Rest);
+    // get extension if there is any
+    while (*Filename != '\0')
+    {
+        // the part behind the point is called extension in windows systems
+        // at least that is what i thought apperantly the '.' is used as part
+        // of the extension too .
+        if (*Filename == '.')
+        {
+            while (*Filename != '\0')
+            {
+                *Extension = *Filename;
+                Extension++;
+                Filename++;
+            }
+        }
+        if (*Filename != '\0')
+        {
+            Filename++;
+        }
+    }
+    *Extension = '\0';
+    return;
+}
+
 #endif
 
+
+
+
+//https://en.cppreference.com/w/cpp/experimental/fs/copy
+bool  fs_copy(const char* source, const char* dest, struct error_code* ec)
+{
+    assert(ec->code == 0);
+
+
+    struct directory_iterator di;
+    if (directory_iterator_init(&di, source, ec) &&
+        fs_create_directory(dest, ec))
+    {
+        do
+        {
+            if (di.bIsDir)
+            {
+                if (strcmp(di.fileName, ".") != 0 &&
+                    strcmp(di.fileName, "..") != 0)
+                {
+                    char origFile[FS_MAX_PATH] = { 0 };
+                    strcpy(origFile, source);
+                    strcat(origFile, "/");
+                    strcat(origFile, di.fileName);
+
+                    char destFile[FS_MAX_PATH] = { 0 };
+                    strcpy(destFile, dest);
+                    strcat(destFile, "/");
+                    strcat(destFile, di.fileName);
+
+
+                    if (!fs_copy(origFile, destFile, ec))
+                    {
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                char origFile[FS_MAX_PATH] = { 0 };
+                strcpy(origFile, source);
+                strcat(origFile, "/");
+                strcat(origFile, di.fileName);
+
+
+                char destFile[FS_MAX_PATH] = { 0 };
+                strcpy(destFile, dest);
+                strcat(destFile, "/");
+                strcat(destFile, di.fileName);
+
+                if (!fs_copy_file(origFile, destFile, ec))
+                {
+                    break;
+                }
+            }
+
+        } while (directory_iterator_next(&di));
+
+        directory_iterator_destroy(&di);
+    }
+
+
+    return ec->code == 0;
+}
